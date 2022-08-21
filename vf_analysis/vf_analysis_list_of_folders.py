a55b2c9bdf472fdad80d7cb57d53acbf9f87542c
YunluZhu <63065013+YunluZhu@users.noreply.github.com>
2022-08-21 15:20:20 -0400

    Update vf_analysis_list_of_folders.py

--- vf_analysis/vf_analysis_list_of_folders.py
+++ vf_analysis/vf_analysis_list_of_folders.py
@@ -1,18 +1,20 @@
 from vf_analysis_by_folder_v4 import vf_analysis
 
 list_of_root_folders = [
-    ("/Volumes/LabData/VF_data_in_use/wt_daylight/organized", 166),                  # WT daylight
-    ("/Volumes/LabData/VF_data_in_use/wt_daylight_finless", 166),                    # WT finless
+    # ("/Volumes/LabData/VF_data_in_use/wt_daylight/organized", 166),                  # WT daylight
+    # ("/Volumes/LabData/VF_data_in_use/wt_daylight_finless", 166),                    # WT finless
     
-    ("/Volumes/LabData/VF_data_in_use/NefmaV4/Longitudinal/long_organized", 166),           # Tau long
-    ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF quantified all", 166),   # Tau SF
-    ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF_LD_combined_7-9only_100mb-plus-singlefish", 166), # Tau SF combined
+    # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Longitudinal/long_organized", 166),           # Tau long
+    # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF quantified all", 166),   # Tau SF
+    # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF_LD_combined_7-9only_100mb-plus-singlefish", 166), # Tau SF combined
+        ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF_LD_combined_7-9only_100mb-plus-singlefish/ld_tau", 166)
     ("/Volumes/LabData/VF_data_in_use/NefmaV4/Tau background/_analyzed", 166),              # Tau bkg
     
-    ("/Volumes/LabData/VF_FA KRH_Tan_lesion/reorganized", 40),   # Tan lesion - FA
-    ("/Volumes/LabData/VF_KR_blind/reorganized", 40),            # blind - KRH
+    # ("/Volumes/LabData/VF_FA KRH_Tan_lesion/reorganized", 40),      # Tan lesion - FA
+    # ("/Volumes/LabData/VF_KRH_blind/reorganized", 40),               # blind - KRH
+    # ("/Volumes/LabData/VF_data_in_use/otog/otog DD", 166),          # otog
     
-    ("/Volumes/LabData/VF_data_in_use/LateralLineHairCell/HC_highFR/HC_organized 220429", 166),   # Hair cell
+    # ("/Volumes/LabData/VF_data_in_use/LateralLineHairCell/HC_highFR/HC_organized 220429", 166),   # Hair cell
 ]
 
 for (root, fr) in list_of_root_folders:
