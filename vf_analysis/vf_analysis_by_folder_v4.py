6056e22f26ab8e44410b4c3e74f33ba14177a44d
YunluZhu <63065013+YunluZhu@users.noreply.github.com>
2022-08-21 15:20:14 -0400

    Update vf_analysis_by_folder_v4.py

--- vf_analysis/vf_analysis_by_folder_v4.py
+++ vf_analysis/vf_analysis_by_folder_v4.py
@@ -1,16 +1,22 @@
 '''
-220502 VF analysis with 
-Take one command line input as the directory containing subfolders with .dlm files
-Call grab_fish_angle and analysis .dlm files in the subfolders
-Store analyzed values of .dlm files in subfolders within subfolders 
-but not concatenate results across .dlm files in different subfolders
-Note: The depth of subfolders containing .dlm files does not matter
+- What is this script
+    This script is part of the Vertical Fish analysis pipeline.
+    It analyzes free-swimming apparatus data (.dlm files), extracts swim bouts and save results as hdf5 files in the same folder
+- How does it work
+    User needs to specify the directory containing .dlm files (or subfolders with .dlm files) and the frame rate used to capture the data.
+    This script runs the "grab_fish_angle" function which calls functions in the <preprocessing> & <bout_analysis> folders
+- How to use it
+    After running the script, user needs to follow instructions and specify data directory and frame rate using CLI
+    Erros may occur if any of the .dlm files to be analyzed lacks a "alignable" swim bout. If this happens, please delete this .dlm file (which is usually unreasonably small) and rerun the script.
+    An "aligned" swim bout contains data 500ms before and 300ms after the time of the peak speed.
+    After running the script, pease refer to catalog.csv fiels for descriptions of the data extracted. A copy of catalog fiels can be found under <docs> folder.
+- Requirments
+    Please refer to the README file for required packages
 '''
 
 import sys
 import os,glob
 from bout_analysis import grab_fish_angle_v4
-# import time
 from tqdm import tqdm
 
 def vf_analysis(root,frame_rate):
@@ -18,29 +24,29 @@ def vf_analysis(root,frame_rate):
     dlm_files_count = 0
     for _, _, files in os.walk(root):
         dlm_files_count = dlm_files_count + len([dlm_files for dlm_files in files if ".dlm" in dlm_files])
-        
+
     all_folders = os.walk(root)
     with tqdm(total=dlm_files_count) as pbar:  # Do tqdm
-         
+
         # determine if dlm is under root folder
         filenames = glob.glob(os.path.join(root,"*.dlm"))
         if filenames:  # if dlm under root, process them
             print(f"\n\n- In {root}")
             grab_fish_angle_v4.run(filenames, root, frame_rate)
             pbar.update(len(filenames)) # update progress bar after processing dlm in the current folder
-            
+
         for path, dir_list, file_list in all_folders: # look for dlm in all subfolders
             # loop through each subfolder
             dir_list.sort()
             for folder_name in dir_list:
-                # get the folder dir by joining path and subfolder name 
-                folder = os.path.join(path, folder_name)        
+                # get the folder dir by joining path and subfolder name
+                folder = os.path.join(path, folder_name)
                 filenames = glob.glob(os.path.join(folder,"*.dlm"))
                 if filenames:
                     print(f"\n\n- In {folder}")
                     grab_fish_angle_v4.run(filenames, folder,frame_rate)
                     pbar.update(len(filenames)) # update progress bar after processing dlm in the current folder
-            
+
 
 if __name__ == "__main__":
     # if want to use Command Line Inputs
