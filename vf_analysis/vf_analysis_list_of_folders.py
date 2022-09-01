e4e7d935e436c96c928675da7477b5d763f6799e
YunluZhu <yunluzhu.g@gmail.com>
2022-09-01 17:26:42 -0400

    Update vf_analysis_list_of_folders.py

--- vf_analysis/vf_analysis_list_of_folders.py
+++ vf_analysis/vf_analysis_list_of_folders.py
@@ -15,21 +15,19 @@ list_of_root_folders = [
     
     # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Longitudinal/long_organized", 166),           # Tau long
     # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF quantified all", 166),   # Tau SF
-    ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF quantified ana", 166),   # SF ana
+    # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF quantified ana", 166),   # SF ana
     # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF_LD_combined_7-9only_100mb-plus-singlefish", 166), # Tau SF combined
-    #     ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF_LD_combined_7-9only_100mb-plus-singlefish/ld_tau", 166)
+        # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Single Fish/SF LD/SF_LD_combined_7-9only_100mb-plus-singlefish/ld_tau", 166)
     # ("/Volumes/LabData/VF_data_in_use/NefmaV4/Tau background/_analyzed", 166),              # Tau bkg
     
-    # ("/Volumes/LabData/VF_FA KRH_Tan_lesion/reorganized", 40),      # Tan lesion - FA
     # ("/Volumes/LabData/VF_KRH_blind/reorganized", 40),               # blind - KRH
-    # ("/Volumes/LabData/VF_data_in_use/otog/otog DD", 166),          # otog
     
     # ("/Volumes/LabData/VF_data_in_use/LateralLineHairCell/HC_highFR/HC_organized 220429", 166),   # Hair cell
     
     # dataset used in method paper
     # ("/Volumes/LabData/manuscript data/2022-07 VF method/behavior data/partial_data_for_ana/WT_DD_7dpf", 166),  # wt
-    # ("/Volumes/LabData/manuscript data/2022-07 VF method/behavior data/partial_data_for_ana/TAN_lesion", 40),   # tan
-    # ("/Volumes/LabData/manuscript data/2022-07 VF method/behavior data/partial_data_for_ana/otog", 166),        # otog
+    ("/Volumes/LabData/manuscript data/2022-07 VF method/behavior data/partial_data_for_ana/TAN_lesion", 40),   # tan
+    ("/Volumes/LabData/manuscript data/2022-07 VF method/behavior data/partial_data_for_ana/otog", 166),        # otog
 ]
 
 for (root, fr) in list_of_root_folders:
